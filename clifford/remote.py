7225dad068cb0edc01077be6e7ea4451a8d496de
Joe Server <joe@jserver.io>
2013-03-20 17:13:28 -0400

    make the remote commands a bit more robust

--- clifford/remote.py
+++ clifford/remote.py
@@ -32,18 +32,19 @@ class BundleInstall(InstanceCommand, SureCheckMixin):
         keydir = self.app.cparser.get('Key Dir', 'keydir')
         if keydir[-1:] == '/':
             keydir = keydir[:-1]
-        bundle = raw_input('Enter name of bundle to install: ')
-        if not bundle or not self.app.cparser.has_option('Bundles', bundle):
+
+        bundle_name = raw_input('Enter name of bundle to install: ')
+        if not bundle_name or not self.app.cparser.has_option('Bundles', bundle_name):
             raise RuntimeError('Bundle not found!')
-        bundle = self.app.cparser.get('Bundles', bundle)
+        bundle = self.app.cparser.get('Bundles', bundle_name)
 
         if instance and bundle and self.sure_check():
-            cmd = 'apt-get -y install %s' % bundle
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(instance.public_dns_name, username='ubuntu', key_filename='%s/%s.pem' % (keydir, instance.key_name))
 
             preseeds = self.get_preseeds(bundle)
+            cmd = 'apt-get -y install %s' % bundle
             if preseeds:
                 stdin, stdout, stderr = ssh.exec_command('cat << EOF | sudo debconf-set-selections\n%s\nEOF' % '\n'.join(preseeds))
                 stdin, stdout, stderr = ssh.exec_command('sudo su -c "DEBIAN_FRONTEND=noninteractive; %s"' % cmd)
@@ -52,8 +53,13 @@ class BundleInstall(InstanceCommand, SureCheckMixin):
             for line in stdout.readlines():
                 if 'is already the newest version' in line:
                     self.app.stdout.write(line)
+            has_error = False
             for line in stderr.readlines():
-                self.app.stdout.write('ERROR: %s' % line)
+                if line.startswith('E: '):
+                    self.app.stdout.write(line)
+                    has_error = True
+            if not has_error:
+                self.app.stdout.write('Installed %s\n' % bundle_name)
             ssh.close()
 
 
@@ -75,11 +81,10 @@ class EasyInstall(InstanceCommand, SureCheckMixin):
         bundle = self.app.cparser.get('Bundles', bundle)
 
         if instance and bundle and self.sure_check():
-            cmd = 'sudo easy_install %s' % bundle
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(instance.public_dns_name, username='ubuntu', key_filename='%s/%s.pem' % (keydir, instance.key_name))
-            stdin, stdout, stderr = ssh.exec_command(cmd)
+            stdin, stdout, stderr = ssh.exec_command('sudo easy_install %s' % bundle)
             for line in stdout.readlines():
                 self.app.stdout.write(line)
             ssh.close()
@@ -110,23 +115,24 @@ class GroupInstall(InstanceCommand, SureCheckMixin):
         keydir = self.app.cparser.get('Key Dir', 'keydir')
         if keydir[-1:] == '/':
             keydir = keydir[:-1]
-        group = raw_input('Enter name of group to install: ')
-        if not group or not self.app.cparser.has_option('Groups', group):
+        group_name = raw_input('Enter name of group to install: ')
+        if not group_name or not self.app.cparser.has_option('Groups', group_name):
             raise RuntimeError('Group not found!')
-        group = self.app.cparser.get('Groups', group)
-        bundles = group.split(' ')
+        group = self.app.cparser.get('Groups', group_name)
+        bundle_names = group.split(' ')
 
-        if instance and bundles and self.sure_check():
+        if instance and bundle_names and self.sure_check():
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(instance.public_dns_name, username='ubuntu', key_filename='%s/%s.pem' % (keydir, instance.key_name))
 
-            for bundle in bundles:
-                if not self.app.cparser.has_option('Bundles', bundle):
+            has_error = False
+            for bundle_name in bundle_names:
+                if not self.app.cparser.has_option('Bundles', bundle_name):
                     continue
-                bundle = self.app.cparser.get('Bundles', bundle)
-                cmd = 'apt-get -y install %s' % bundle
+                bundle = self.app.cparser.get('Bundles', bundle_name)
                 preseeds = self.get_preseeds(bundle)
+                cmd = 'apt-get -y install %s' % bundle
                 if preseeds:
                     stdin, stdout, stderr = ssh.exec_command('cat << EOF | sudo debconf-set-selections\n%s\nEOF' % '\n'.join(preseeds))
                     stdin, stdout, stderr = ssh.exec_command('sudo su -c "DEBIAN_FRONTEND=noninteractive; %s"' % cmd)
@@ -136,7 +142,14 @@ class GroupInstall(InstanceCommand, SureCheckMixin):
                     if 'is already the newest version' in line:
                         self.app.stdout.write(line)
                 for line in stderr.readlines():
-                    self.app.stdout.write('ERROR: %s' % line)
+                    if line.startswith('E: '):
+                        self.app.stdout.write(line)
+                        has_error = True
+                if has_error:
+                    ssh.close()
+                    raise RuntimeError("Unable to continue...")
+                self.app.stdout.write('Installed %s\n' % bundle_name)
+
             ssh.close()
 
 
@@ -179,6 +192,8 @@ class Script(InstanceCommand, SureCheckMixin):
                 stdin, stdout, stderr = ssh.exec_command('chmod 744 clifford_script.sh; ./clifford_script.sh')
                 for line in stdout.readlines():
                     self.app.stdout.write(line)
+                for line in stderr.readlines():
+                    self.app.stdout.write('ERROR: %s' % line)
                 ssh.close()
 
 
@@ -196,11 +211,45 @@ class Upgrade(InstanceCommand, SureCheckMixin):
             keydir = keydir[:-1]
 
         if instance and self.sure_check():
-            cmd = 'sudo apt-get -y update; sudo apt-get -y upgrade'
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(instance.public_dns_name, username='ubuntu', key_filename='%s/%s.pem' % (keydir, instance.key_name))
-            stdin, stdout, stderr = ssh.exec_command(cmd)
-            for line in stdout.readlines():
-                self.app.stdout.write(line)
+
+            has_error = False
+            stdin, stdout, stderr = ssh.exec_command('sudo su -c "apt-get -y update"')
+            for line in stderr.readlines():
+                if line.startswith('E: '):
+                    self.app.stdout.write(line)
+                    has_error = True
+            if has_error:
+                raise RuntimeError("Unable to continue...")
+            self.app.stdout.write('UPDATED\n')
+
+            upgrade = raw_input('Do you want to upgrade? ')
+            if upgrade.lower() in ['y', 'yes']:
+                stdin, stdout, stderr = ssh.exec_command('sudo su -c "env DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::Options::=--force-confnew upgrade"')
+                for line in stderr.readlines():
+                    if line.startswith('E: '):
+                        self.app.stdout.write(line)
+                        has_error = True
+                if has_error:
+                    raise RuntimeError("Unable to continue...")
+                self.app.stdout.write('UPGRADED\n')
+
+            dist_upgrade = raw_input('Do you want to dist-upgrade? ')
+            if dist_upgrade.lower() in ['y', 'yes']:
+                stdin, stdout, stderr = ssh.exec_command('sudo su -c "env DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::Options::=--force-confnew dist-upgrade"')
+                for line in stderr.readlines():
+                    if line.startswith('E: '):
+                        self.app.stdout.write(line)
+                        has_error = True
+                if has_error:
+                    raise RuntimeError("Unable to continue...")
+                self.app.stdout.write('DIST-UPGRADED\n')
+
             ssh.close()
+
+            reboot = raw_input('Do you want to reboot? ')
+            if reboot.lower() in ['y', 'yes']:
+                self.app.stdout.write('Rebooting...\n')
+                instance.reboot()
